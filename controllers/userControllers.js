194b0db16dcd192555cb565c6a3de91691f71c4e
Vedang Dhuri <netvedang@gmail.com>
2026-01-23 21:14:01 +0530

    #	modified:   controllers/userControllers.js #	modified:   server.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -49,4 +49,42 @@ export const registerUser = async (req, res) => {
   }
 };
 
+export const verification = async (req, res) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if(!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).json({
+        success: false,
+        message: 'Authorization Token is missing or invalid'
+      
+      })
+    }
+    const token = authHeader.split(' ')[1]
+
+    let decoded;
+
+    try {
+      decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+    } catch (error) {
+      return res.status(401).json({
+        success: false,
+        message: 'Invalid token'
+      })
+    }
+
+    user.token = null;
+    user.isVerified = true;
+    await user.save();
+
+    return res.status(200).json({
+      success: true,
+      message: 'Email verified successfully'
+    })
+  } catch(error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message
+    })
+  }
+}
 
