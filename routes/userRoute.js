e730a474e198637ee71e11ea643d0fef487c5a6b
Vedang Dhuri <netvedang@gmail.com>
2026-01-23 23:46:08 +0530

    modified file

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { registerUser } from '../controllers/userControllers.js';
+import { verification } from '../controllers/userControllers.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
+router.post('/verify', verification);
 
 export default router;
