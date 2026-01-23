489cac14e0d615bd3a962179532ec843e9824df1
Vedang Dhuri <netvedang@gmail.com>
2026-01-24 01:57:37 +0530

    modified file, 1-57-07

--- database/db.js
+++ database/db.js
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGO_URI}/note-app`);
-        console.log('MongoDB Connected Successfully');
-    } catch(error) {
-        console.error('MongoDb Connection Error:', error);
-    }
-}
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/note-app`);
+    console.log("MongoDB Connected Successfully");
+  } catch (error) {
+    console.error("MongoDb Connection Error:", error);
+  }
+};
 
 export default connectDB;
